a4d6b966910eeb39c8e2d3c0a7bfac26e892d704
Christoph Kappel <christoph@unexist.dev>
2021-04-03 12:21:29 +0100

    Added animation handling

--- python/birdhouse.py
+++ python/birdhouse.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3 
+#!/usr/bin/python3
 
 ##
 # @package birdhouse
@@ -7,16 +7,19 @@
 # @copyright 2021 Christoph Kappel <christoph@unexist.dev>
 # @version $Id$
 #
-# This program can be distributed under the terms of the GNU GPLv2.
+# This program can be distributed under the terms of the GNU GPLv3.
 # See the file LICENSE for details.
 ##
 
-from time import sleep
+from time import sleep, time
+from copy import copy
+from glop import glob
 import os
-import copy
 import logging
+
 from telegram import Update
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext, PicklePersistence
+
 import RPi.GPIO as GPIO
 from picamera import PiCamera
 
@@ -24,70 +27,99 @@ from picamera import PiCamera
 SENSOR_PIN = 23
 CAMERA = None
 BOT = None
-UDATA = {}
 
-def motion_callback(channel):
-    if 0 < len(UDATA):
-        print("Taking photo")
-        CAMERA.start_preview()
-        sleep(1)
-        CAMERA.capture(os.getcwd() + "/image.jpg")
-        CAMERA.stop_preview()
-
-        for userid, username in UDATA.items():
-            print("Send message to %s" % username)
-            BOT.send_message(chat_id=userid, text="Chirp! Chirp!")
-            BOT.send_photo(chat_id=userid, photo=open(os.getcwd() + "/image.jpg", "rb"))
-     
-# Enable logging
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
 )
 
-logger = logging.getLogger(__name__)
+LOGGER = logging.getLogger(__name__)
+
+# Event handler
+def alarm(context: CallbackContext) -> None:
+    LOGGER.info("Alarm called - scanning files")
+
+    imgList = glob(os.getcwd() + "/image-*.jpg")
+
+    if 1 < len(imgList):
+        LOGGER.info("Converting {:d} images..", len(imgList))
+        system("convert -delay 5 -loop 0 image-*.jpg anim.gif")
+
+    # Send to subscribers
+    for userid, username in context.bot.user_data.items():
+        LOGGER.info("Send message to %s", username)
+        BOT.send_message(chat_id=userid, text="Chirp! Chirp!")
+
+        # Select file
+        if os.path.isFile("anim.gif"):
+            BOT.send_animation(chat_id=userid, animation=open("anim.gif", "rb"))
+            os.remove("anim.gif")
+        else:
+            for img in imgList:
+                BOT.send_photo(chat_id=userid, photo=open(img, "rb"))
+                os.remove(img)
+
+def motion_callback(channel):
+    # Take photo for current timestamp
+    LOGGER.info("Taking photo")
+    CAMERA.start_preview()
+    sleep(1)
+    CAMERA.capture(getcwd() + "/image-{:d}.jpg".format(int(time())))
+    CAMERA.stop_preview()
+
+    # Schedule job
+    for job in BOT.job_queue.get_jobs_by_name("motion"):
+        LOGGER.info("Rescheduling alarm..")
+        job.schedule_removal()
+
+    BOT.job_queue.run_once(alarm, 3, context=None, name="motion")
 
 # Commands
-def start_command(update: Update, context: CallbackContext) -> None:
+def sub_command(update: Update, context: CallbackContext) -> None:
     update.message.reply_text("Hi %s, thanks for the subscription!" % update.message.from_user.username)
 
     # Store user
     context.user_data[update.message.from_user.id] = update.message.from_user.username
-    UDATA[update.message.from_user.id] = update.message.from_user.username
 
-    print("%s subscribed to updates" % update.message.from_user.username)
+    LOGGER.info("%s subscribed to updates", update.message.from_user.username)
 
-def stop_command(update: Update, context: CallbackContext) -> None:
+def unsub_command(update: Update, context: CallbackContext) -> None:
     update.message.reply_text("Bye bye %!" % update.message.from_user.username)
 
     # Delete user
     del context.user_data[update.message.from_user.id]
-    del UDATA[update.message.from_user.id]
 
-    print("%s unsubscribed from updates" % update.message.from_user.username)
+    LOGGER.info("%s unsubscribed from updates", update.message.from_user.username)
 
 def pic_command(update: Update, context: CallbackContext) -> None:
     update.message.reply_text("Say cheese!")
 
-    print("Taking photo")
+    LOGGER.info("Taking photo")
     CAMERA.start_preview()
     sleep(1)
-    CAMERA.capture(os.getcwd() + "/image.jpg")
+    CAMERA.capture(os.getcwd() + "/pic.jpg")
     CAMERA.stop_preview()
 
-    context.bot.send_photo(chat_id=update.message.from_user.id, photo=open(os.getcwd() + "/image.jpg", "rb"))
+    context.bot.send_photo(chat_id=update.message.from_user.id, photo=open(os.getcwd() + "/pic.jpg", "rb"))
+
+def die_command(update: Update, context: CallbackContext) -> None:
+    # Send to subscribers
+    for userid, username in context.user_data.items():
+        LOGGER.info("Send message to %s" % username)
+        context.bot.send_message(chat_id=userid, text="Going back to slumber! zZzZ")
 
 def help_command(update: Update, _: CallbackContext) -> None:
     update.message.reply_text(
         "Available commands:"
-        "/start - Subcribe to updates"
-        "/stop  - Unsubscribe from updates"
+        "/sub   - Subcribe to updates"
+        "/unsub - Unsubscribe from updates"
         "/pic   - Take a picture"
+        "/die   - Shutdown"
     )
 
 if __name__ == "__main__":
     # Sanity check
     if os.environ.get("TOKEN") is None:
-        print("Env var TOKEN is required!")
+        LOGGER.info("Env var TOKEN is required!")
         exit()
 
     # Set up cam
@@ -108,16 +140,16 @@ if __name__ == "__main__":
     # Add dispatcher commands
     dispatcher = updater.dispatcher
 
-    dispatcher.add_handler(CommandHandler("start", start_command))
-    dispatcher.add_handler(CommandHandler("stop", stop_command))
+    dispatcher.add_handler(CommandHandler("sub", sub_command))
+    dispatcher.add_handler(CommandHandler("unsub", unsub_command))
     dispatcher.add_handler(CommandHandler("pic", pic_command))
+    dispatcher.add_handler(CommandHandler("die", die_command))
     dispatcher.add_handler(CommandHandler("help", help_command))
 
     updater.start_polling()
 
     # FIXME: STUPID!
-    BOT = copy.copy(updater.bot)
-    UDATA = copy.copy(dispatcher.user_data)
+    BOT = copy(updater.bot)
 
     # Say all known users hello
     for userid, username in dispatcher.user_data.items():
